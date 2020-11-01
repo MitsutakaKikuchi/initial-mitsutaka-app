eabef31d677c2e590fd9a70637908bb8cb4b5c2d
MitsutakaKikuchi <kiku.mitsu.yoshi.taka@gmail.com>
2020-11-01 22:24:37 +0900

    a

--- db/migrate/20200916145654_type_change_user2_user_id.rb
+++ db/migrate/20200916145654_type_change_user2_user_id.rb
@@ -1,5 +1,5 @@
 class TypeChangeUser2UserId < ActiveRecord::Migration[6.0]
   def change
-    change_column :users2s, :user_id, 'integer USING CAST(user_id AS integer)'
+    change_column :users2s, :user_id, 'integer USING CAST(hoge AS integer)'
   end
 end
